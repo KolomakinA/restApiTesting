6f8edb9fe00e7254821aeffe554184ef185bde75
olazarenko <olga_l@ukr.net>
2017-02-10 15:21:47 +0200

    FailedTaskCollection

--- src/main/java/com/serena/rlc/tests/TestRunner.java
+++ src/main/java/com/serena/rlc/tests/TestRunner.java
@@ -15,12 +15,8 @@ public class TestRunner {
         String mockProviderName="M";
         String provInstUUID = execObject.findProviders(mockProviderName);//get execition provider instance UUID
         String duProvInstUUID = duObject.findDUProvidersAndreturnUUID(mockProviderName);//get DU provider instance UUID
-
-
-        String tcID = rp.postTaskCollection("My super TC 1");//Creating a task collection
-        System.out.println("tcid =" + tcID);
+       String tcID = rp.postTaskCollection("My super TC 1");//Creating a task collection
         String jsonFromGetTC = rp.getTaskCollections(tcID);//Getting list of task collections
-
 //        if (jsonFromGetTC.contains(tcID)){//the list should contain a ID of our task collection
 //            //creating a task for the task collection, using TC id, provider instance UUID and a task name
 //            rp.addTasktoTCid(tcID, provInstUUID,"Rest task Name");
@@ -29,37 +25,23 @@ public class TestRunner {
 //            System.out.println("We were not able to find TaskCollectionID in the list of existing TaskCollection");
 //            System.exit(1);
 //        }
-
         rp.addTasktoTCid(tcID,provInstUUID,"Rest task Name");
-        //Create Task POST: http://stl-qa-oalmt3/rlc/rest/v2/taskcollections/100089/tasks?
-
-        //CreateRunforTaskClooection  POST http://stl-qa-oalmt3/rlc/rest/v2/taskcollections/100089/executions?
-        // {"environment": {"environment_id": "1012:1"	}}
         rp.createRunforTaskClooection(tcID);
-
-        //Receive number of the Run
         rp.receiveNumberofTCRUN();
-
-        //Start TaskCollectionRun PUT: http://stl-qa-oalmt3/rlc/rest/v2/taskcollections/100089/executions/20/start?
         rp.srartTCRun(tcID);
-
-        //Check Taskcollection Run status GET:  http://stl-qa-oalmt3/rlc/rest/v2/taskcollections/100089/executions/20/
         rp.checkRunExecution(tcID);
 
 //Failed Task Collection
         V2taskcollections rpFail = new V2taskcollections();
-        String tcID2 = rpFail.postTaskCollection("_");
+        String tcID2 = rpFail.postTaskCollection("TC dva");
         rpFail.getTaskCollections(tcID2);
-        rpFail.addTaskWithParm(tcID2, provInstUUID,"Task With Parm");
-        rpFail.addDUsToTC(tcID2, duProvInstUUID);
-        String id = getDUsforTC(tcID2);
-        rpFail.relateDU(tcID2,id);
+        String taskID = rpFail.addTaskWithParm(tcID2, provInstUUID,"Task With Parm");
+        rpFail.addDUsToTC(tcID2,provInstUUID);
+        String duID = rpFail.getDUsforTC(tcID2);
+        rpFail.relateDU(tcID2, taskID,duID);
         rpFail.createRunforTaskClooection(tcID2);
         rpFail.srartTCRun(tcID2);
         rpFail.checkRunExecution(tcID2);
-
-
-
     }
 }
 
--- src/main/java/com/serena/rlc/tests/V2taskcollections.java
+++ src/main/java/com/serena/rlc/tests/V2taskcollections.java
@@ -17,12 +17,12 @@ import java.io.IOException;
 /**
  * Created by akolomakin on 07.02.2017.
  */
-public class V2taskcollections extends ConfigurationProperties{
+public class V2taskcollections extends ConfigurationProperties {
     int nubmerRun;
     ConfigurationProperties prop = new ConfigurationProperties();
 
     public V2taskcollections() {
-       this.prop = new ConfigurationProperties();
+        this.prop = new ConfigurationProperties();
         prop.setRlcURL("http://stl-qa-oalmt3/");
         prop.setSbmUserName("admin");
         prop.setSbmUserPass("");
@@ -30,160 +30,87 @@ public class V2taskcollections extends ConfigurationProperties{
 
     }
 
-//actually we cannot delete task collections directly - otherwise we get sql constrain violation
+    //actually we cannot delete task collections directly - otherwise we get sql constrain violation
     public void deleteTaskCollection(String tcId) throws IOException {
         String resURI = "rlc/rest/v2/taskcollections/" + tcId;
-        String url = prop.getRlcURL()+ resURI;
-        Del delete= new Del(url);
+        String url = prop.getRlcURL() + resURI;
+        Del delete = new Del(url);
         String response = delete.httpDel();
         System.out.println(response);
     }
 
-     //Get list of task collections
-//    public String getTaskCollections() throws  IOException{
-//        String resURI = "rlc/rest/v2/taskcollections/";
-//        String url = prop.getRlcURL()+ resURI;
-//        Get httpGet = new Get(url);
-//        return httpGet.httpGet();
-//
-//    }
-
     //get particular task collection
-    public String getTaskCollections(String tcid) throws  IOException {
+    public String getTaskCollections(String tcid) throws IOException {
         String resURI = "rlc/rest/v2/taskcollections/" + tcid;
         String url = prop.getRlcURL() + resURI;
         Get httpGet = new Get(url);
-        //System.out.println(url + httpGet.httpGet());
+        // System.out.println(url + httpGet.httpGet());
         return httpGet.httpGet();
 
     }
 
-
     //Create task collection with a specified name
-    public String postTaskCollection(String tc_name) throws IOException{// String env_id, String env_name
+    public String postTaskCollection(String tc_name) throws IOException {// String env_id, String env_name
         Utils utils = new Utils();
         String file = utils.readFile("resources\\jsonTemplates\\postTaskCollection.json");//read a json template
         file = file.replace("%TC_NAME%", tc_name);//put task collection name in to template
 
         String resURI = "rlc/rest/v2/taskcollections/";//making an URL
-        String url = prop.getRlcURL()+ resURI;
+        String url = prop.getRlcURL() + resURI;
 
-        Post httpPost = new Post(url,file);//executing POST
+        Post httpPost = new Post(url, file);//executing POST
         String postResult = httpPost.httpPost();
 
         JSONObject jso = new JSONObject(postResult);
         //System.out.println("postTaskCollection" + jso);
         prop.setCreatedTCId(jso.getJSONObject("localReturn").get("id").toString());
         // /updating global variable and returning the task collection ID
-        return jso.getJSONObject("localReturn").get("id").toString();
+        String getTCID = jso.getJSONObject("localReturn").get("id").toString();
+        System.out.println("TCID =" + getTCID);
+        return getTCID;
     }
 
     public String addTasktoTCid(String tcID, String provInstUUID, String taskName) throws IOException {
         String resURI = "rlc/rest/v2/taskcollections/" + tcID + "/tasks";//making an URL
-        String url = prop.getRlcURL()+ resURI;
+        String url = prop.getRlcURL() + resURI;
 
         Utils utils = new Utils();
         String file = utils.readFile("resources\\jsonTemplates\\createTaskForMockProvider.json");//read a json template
         file = file.replace("%Prov_Inst_UUID%", provInstUUID);
         file = file.replace("%Env_ID%", prop.getMockEnvironmentId());
         file = file.replace("%Task_Title%", taskName);
-        Post httpPost = new Post(url,file);//forming POST
+        Post httpPost = new Post(url, file);//forming POST
         String postResult = httpPost.httpPost();//executing POST
         JSONObject jso = new JSONObject(postResult);
         return jso.getJSONObject("localReturn").get("id").toString();
 
     }
 
-    public String addTaskWithParm(String tcID2, String provInstUUID, String taskName) throws Exception{
+    public String addTaskWithParm(String tcID2, String provInstUUID, String taskName) throws Exception {
         String resURI = "rlc/rest/v2/taskcollections/" + tcID2 + "/tasks";//making an URL
-        String url = prop.getRlcURL()+ resURI;
+        String url = prop.getRlcURL() + resURI;
         Utils util = new Utils();
         String file = util.readFile("resources\\jsonTemplates\\createTaskWithParm.json");//read a json template
         file = file.replace("%Prov_Inst_UUID%", provInstUUID);
         file = file.replace("%Env_ID%", prop.getMockEnvironmentId());
         file = file.replace("%Task_Title%", taskName);
-        Post httpPost = new Post(url,file);//forming POST
+        Post httpPost = new Post(url, file);//forming POST
         String postResult = httpPost.httpPost();//executing POST
-        return postResult;
-    }
-
-    public void relateDU(String tcId2, int DUid) throws Exception{
-        //http://stl-qa-oalmt3/rlc/rest/v2/taskcollections/100177/tasks/139   PUT {"deployment_unit":[{"id":3}]}
-        String resURI = "rlc/rest/v2/taskcollections/" + tcId2 + "/task/" + DUid;
-        String url = prop.getRlcURL()+ resURI;
-        String parm = "{\"deployment_unit\":[{\"id\":3}]}";
-
-        Put relateDU = new Put(url,parm);
-        relateDU.httpPut();
-        return;
-    }
-
-    public String createRunforTaskClooection(String tcID) throws IOException{
-        // POST http://stl-qa-oalmt3/rlc/rest/v2/taskcollections/100089/executions?
-        // {"environment": {"environment_id": "1012:1"	}}
-
-        String urlTemp = "rlc/rest/v2/taskcollections/" + tcID + "/executions";
-        String url = prop.getRlcURL()+ urlTemp;
-
-        Utils utils = new Utils();
-        String link = utils.readFile("resources\\jsonTemplates\\startRun.json");
-
-        link = link.replace("%Env_ID%",prop.getMockEnvironmentId());
-        Post run = new Post(url,link);
-        try {
-            String postResult = run.httpPost();
-            //System.out.println("res" + postResult);
-
-            JSONObject jsonRun = new JSONObject(postResult);
-
-            nubmerRun = Integer.parseInt(jsonRun.getJSONObject("localReturn").get("id").toString());
-            System.out.println("nubmerRun: "+ nubmerRun);
-            return postResult;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return "Error";
-        }
-    }
-
-    public int receiveNumberofTCRUN(){
-        return nubmerRun;
-    }
-
-    public void srartTCRun(String tcId) throws IOException{
-        //PUT: http://stl-qa-oalmt3/rlc/rest/v2/taskcollections/100089/executions/20/start?
-        String resURI = "rlc/rest/v2/taskcollections/" + tcId + "/executions/" + nubmerRun +"/start";
-        String url = prop.getRlcURL()+ resURI;
-
-        Put srartRun = new Put(url,"");
-        srartRun.httpPut();
-        System.out.println("START");
-            }
-
-    public boolean checkRunExecution(String tcId) throws Exception {
-        //http://stl-qa-oalmt3/rlc/rest/v2/taskcollections/100089/executions/20/
-        String resURI = "rlc/rest/v2/taskcollections/" + tcId + "/executions/" + nubmerRun;
-        String url = prop.getRlcURL()+ resURI;
-        Thread.sleep(10000);
-        Get statusRun = new Get(url);
-        String obStat = statusRun.httpGet();
-        //System.out.println(url);
-        JSONObject jsonRun = new JSONObject(obStat);
-        if (jsonRun.getJSONObject("localReturn").get("status").equals("SUCCEED")){
-            System.out.println("TC run is Completed");
-        } else System.out.println("TC run is RUNING");;
-
-        return true;
+        JSONObject jso = new JSONObject(postResult);
+        String taskID = jso.getJSONObject("localReturn").get("id").toString();
+        System.out.println("TaskId: " + taskID);
+        return jso.getJSONObject("localReturn").get("id").toString();
     }
 
     public String addDUsToTC(String tcId, String duProvInstUUID) throws IOException {
         String resURI = "rlc/rest/v2/taskcollections/" + tcId + "/integration-entities";
-        String url = prop.getRlcURL()+ resURI;
+        String url = prop.getRlcURL() + resURI;
 
         Utils utils = new Utils();
         String file = utils.readFile("resources\\jsonTemplates\\addSnapshotDusToaTC.json");//read a json template
         file = file.replace("%Prov_Inst_UUID%", duProvInstUUID);
 
-        Post httpPost = new Post(url,file);//executing POST
+        Post httpPost = new Post(url, file);//executing POST
         String postResult = httpPost.httpPost();
 
         return postResult;
@@ -191,14 +118,89 @@ public class V2taskcollections extends ConfigurationProperties{
 
     public String getDUsforTC(String tcId) throws IOException {
         String resURI = "rlc/rest/v2/taskcollections/" + tcId + "/integration-entities?tagsGroupExpression=deployment_unit";
-        String url = prop.getRlcURL()+ resURI;
+        String url = prop.getRlcURL() + resURI;
 
         Get httpGet = new Get(url);
         String getResult = httpGet.httpGet();
 
         JSONObject jsno = new JSONObject(getResult);
+        String duID = jsno.getJSONArray("localReturn").getJSONObject(0).get("id").toString();
+        System.out.println("duID = " + duID);
+        return duID;
+    }
 
-        return jsno.getJSONArray("localReturn").getJSONObject(0).get("id").toString();
+    public void relateDU(String tcId2, String duID, String taskID) throws Exception {
+        //http://stl-qa-oalmt3/rlc/rest/v2/taskcollections/100177/tasks/139   PUT {"deployment_unit":[{"id":3}]}
+        String resURI = "rlc/rest/v2/taskcollections/" + tcId2 + "/task/" + taskID;
+        String url = prop.getRlcURL() + resURI;
+        Utils utils = new Utils();
+        String parm = utils.readFile("resources\\jsonTemplates\\addSnapshotDusToaTC.json");//read a json template
+        parm = parm.replace("%DU_ID%", duID);
+        Put relateDU = new Put(url, parm);
+        relateDU.httpPut();
+        return;
+    }
+
+    public String createRunforTaskClooection(String tcID) throws IOException {
+        // POST http://stl-qa-oalmt3/rlc/rest/v2/taskcollections/100089/executions?
+        // {"environment": {"environment_id": "1012:1"	}}
+
+        String urlTemp = "rlc/rest/v2/taskcollections/" + tcID + "/executions";
+        String url = prop.getRlcURL() + urlTemp;
+
+        Utils utils = new Utils();
+        String link = utils.readFile("resources\\jsonTemplates\\startRun.json");
+
+        link = link.replace("%Env_ID%", prop.getMockEnvironmentId());
+        Post run = new Post(url, link);
+        try {
+            String postResult = run.httpPost();
+            //System.out.println("res" + postResult);
+
+            JSONObject jsonRun = new JSONObject(postResult);
+
+            nubmerRun = Integer.parseInt(jsonRun.getJSONObject("localReturn").get("id").toString());
+            System.out.println("nubmerRun: " + nubmerRun);
+            return postResult;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Error";
+        }
+    }
+
+    public int receiveNumberofTCRUN() {
+        return nubmerRun;
+    }
+
+    public void srartTCRun(String tcId) throws IOException {
+        //PUT: http://stl-qa-oalmt3/rlc/rest/v2/taskcollections/100089/executions/20/start?
+        String resURI = "rlc/rest/v2/taskcollections/" + tcId + "/executions/" + nubmerRun + "/start";
+        String url = prop.getRlcURL() + resURI;
+
+        Put srartRun = new Put(url, "");
+        srartRun.httpPut();
+        System.out.println("START");
+    }
+
+    public void checkRunExecution(String tcId) throws Exception {
+        //http://stl-qa-oalmt3/rlc/rest/v2/taskcollections/100089/executions/20/
+        String resURI = "rlc/rest/v2/taskcollections/" + tcId + "/executions/" + nubmerRun;
+        String url = prop.getRlcURL() + resURI;
+        Thread.sleep(5555);
+        Get statusRun = new Get(url);
+        String obStat = statusRun.httpGet();
+        JSONObject jsonRun = new JSONObject(obStat);
+        //System.out.println(url + jsonRun);
+        Object status = jsonRun.getJSONObject("localReturn").get("status");
+        System.out.println(status);
+        if (status.equals("SUCCEED")) {
+            System.out.println("***** TC run is Completed*****");
+            return;
+        }
+        if (jsonRun.getJSONObject("localReturn").get("status").equals("FAILED")) {
+            System.out.println("***** Task collection is FAILED ^( ***** ");
+            return;
+        } else System.out.println(" ***** TC run is RUNING? ******");
     }
 }
 
